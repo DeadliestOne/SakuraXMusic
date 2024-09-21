ecad64dc6e14245a46b9c51645f69ec51c7e42ab
DeadliestOne <uncountableaura@gmail.com>
2024-09-22 01:49:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -78,13 +78,13 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://ibb.co/5GkX81x"
+    "START_IMG_URL", "https://ibb.co/dfBQxBR"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://ibb.co/5GkX81x"
+    "PING_IMG_URL", "https://ibb.co/jDsCn9x"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://ibb.co/5GkX81x"
+STATS_IMG_URL = "https://ibb.co/LJ4Vjvt"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"

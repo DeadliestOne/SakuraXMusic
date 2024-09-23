01223c9a3ab5d109cb8ef7fae354890a0256b770
DeadliestOne <uncountableaura@gmail.com>
2024-09-24 00:37:40 +0530

    Update ping.py

--- AnonXMusic/plugins/tools/ping.py
+++ AnonXMusic/plugins/tools/ping.py
@@ -2,27 +2,28 @@ from datetime import datetime
 
 from pyrogram import filters
 from pyrogram.types import Message
-
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
+from config import *
 from AnonXMusic import app
-from AnonXMusic.core.call import Anony
+from AnonXMusic.core.call import DAXX
 from AnonXMusic.utils import bot_sys_stats
 from AnonXMusic.utils.decorators.language import language
 from AnonXMusic.utils.inline import supp_markup
-from config import BANNED_USERS, PING_IMG_URL
+from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
+@app.on_message(filters.command("ping", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
     start = datetime.now()
-    response = await message.reply_photo(
-        photo=PING_IMG_URL,
+    response = await message.reply_video(
+        video="https://telegra.ph/file/45b3b3f0a215eb080b72a.mp4",
         caption=_["ping_1"].format(app.mention),
     )
-    pytgping = await Anony.ping()
+    pytgping = await DAXX.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
         _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
         reply_markup=supp_markup(_),
-    )
+                  )

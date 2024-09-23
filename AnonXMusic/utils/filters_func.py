7903d12efb8870ced3399ab0039165f455c9142a
DeadliestOne <uncountableaura@gmail.com>
2024-09-24 00:16:20 +0530

    Update filters_func.py

--- AnonXMusic/utils/filters_func.py
+++ AnonXMusic/utils/filters_func.py
@@ -1,8 +1,8 @@
 from enum import Enum, auto
-from ANNIEMUSIC import app
+from AnonXMusic import app
 from pyrogram.types import InlineKeyboardMarkup, Message
-from ANNIEMUSIC.utils.msg_types import button_markdown_parser
-from ANNIEMUSIC.utils.notes_func import NoteFillings
+from AnonXMusic.utils.msg_types import button_markdown_parser
+from AnonXMusic.utils.notes_func import NoteFillings
 from emojis import decode
 from pyrogram.types import Message
 

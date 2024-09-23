109e9778c4b5425eb140e7a61075caa821d9fd75
DeadliestOne <uncountableaura@gmail.com>
2024-09-24 00:18:15 +0530

    Update notes_func.py

--- AnonXMusic/utils/notes_func.py
+++ AnonXMusic/utils/notes_func.py
@@ -3,10 +3,10 @@ from pyrogram.types import Message
 import html
 import re
 from pyrogram.types import InlineKeyboardMarkup
-from ANNIEMUSIC import app
-from ANNIEMUSIC.mongo.notesdb import GetNote
+from AnonXMusic import app
+from AnonXMusic.mongo.notesdb import GetNote
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from ANNIEMUSIC.utils.msg_types import button_markdown_parser
+from AnonXMusic.utils.msg_types import button_markdown_parser
 
 class NoteTypeMap(Enum):
     text = auto()

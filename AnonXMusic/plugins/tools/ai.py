6124dbea2737c75b36fbe31ead3e95abba8ea012
DeadliestOne <uncountableaura@gmail.com>
2024-09-24 21:11:49 +0530

    Update ai.py

--- AnonXMusic/plugins/tools/ai.py
+++ AnonXMusic/plugins/tools/ai.py
@@ -5,7 +5,7 @@ import os
 from typing import Union, Tuple, List
 from pyrogram import filters, types as t
 from lexica import AsyncClient
-from ANNIEMUSIC import app
+from AnonXMusic import app
 from lexica.constants import languageModels
 
 

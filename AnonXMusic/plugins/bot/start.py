2d1fd3f6095301d0ee9210da30283ebc93a958ca
DeadliestOne <uncountableaura@gmail.com>
2024-09-24 21:28:17 +0530

    Update start.py

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -12,7 +12,7 @@ from AnonXMusic.misc import _boot_
 from AnonXMusic.plugins.sudo.sudoers import sudoers_list
 from AnonXMusic.utils.database import get_served_chats, get_served_users, get_sudoers
 from AnonXMusic.utils import bot_sys_stats
-from AnonXMusicC.utils.database import (
+from AnonXMusic.utils.database import (
     add_served_chat,
     add_served_user,
     blacklisted_chats,

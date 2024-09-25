c08cd203c9bc97a85fdc60166593079fd7a4d519
DeadliestOne <uncountableaura@gmail.com>
2024-09-25 21:21:01 +0530

    Update stats.py

--- AnonXMusic/utils/inline/stats.py
+++ AnonXMusic/utils/inline/stats.py
@@ -1,36 +1,45 @@
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-def speed_markup(_, chat_id):
+def stats_buttons(_, status):
+    not_sudo = [
+        InlineKeyboardButton(
+            text=_["SA_B_1"],
+            callback_data="TopOverall",
+        )
+    ]
+    sudo = [
+        InlineKeyboardButton(
+            text=_["SA_B_2"],
+            callback_data="bot_stats_sudo",
+        ),
+        InlineKeyboardButton(
+            text=_["SA_B_3"],
+            callback_data="TopOverall",
+        ),
+    ]
+    upl = InlineKeyboardMarkup(
+        [
+            sudo if status else not_sudo,
+            [
+                InlineKeyboardButton(
+                    text=_["CLOSE_BUTTON"],
+                    callback_data="close",
+                ),
+            ],
+        ]
+    )
+    return upl
+
+
+def back_stats_buttons(_):
     upl = InlineKeyboardMarkup(
         [
             [
                 InlineKeyboardButton(
-                    text="🕒 0.5x",
-                    callback_data=f"SpeedUP {chat_id}|0.5",
+                    text=_["BACK_BUTTON"],
+                    callback_data="stats_back",
                 ),
-                InlineKeyboardButton(
-                    text="🕓 0.75x",
-                    callback_data=f"SpeedUP {chat_id}|0.75",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["P_B_4"],
-                    callback_data=f"SpeedUP {chat_id}|1.0",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text="🕤 1.5x",
-                    callback_data=f"SpeedUP {chat_id}|1.5",
-                ),
-                InlineKeyboardButton(
-                    text="🕛 2.0x",
-                    callback_data=f"SpeedUP {chat_id}|2.0",
-                ),
-            ],
-            [
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
                     callback_data="close",
